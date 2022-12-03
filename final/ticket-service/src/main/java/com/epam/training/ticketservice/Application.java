721fac5386d9e9998b636cfd5621b62a9d792904
lipeczpeti <lipecz.p@gmail.com>
2022-12-03 15:55:36 +0100

    Spring Boot added to Application

--- final/ticket-service/src/main/java/com/epam/training/ticketservice/Application.java
+++ final/ticket-service/src/main/java/com/epam/training/ticketservice/Application.java
@@ -1,6 +1,16 @@
 package com.epam.training.ticketservice;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+@SpringBootApplication
 public class Application {
+
     public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
     }
 }
